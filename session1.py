f7d6dd5908980d6b79500e86430f9d565d9aea0e
ethanblairs <ethanblairs2006@gmail.com>
2026-01-20 16:59:17 +0100

    My first commit ever!!!

---
+++ session1.py
@@ -0,0 +1,13 @@
+print("hello world")
+print('hello world')
+print("7+6")
+print("program")
+
+print("3 + 4 =", 3+4, "and 5 x 6=", 5*6)
+print("I have $", 200+100, ".")
+print("I have $", 200+100, ".", sep="")
+
+print("1", end="")
+print("2", end="")
+print("3")
+
